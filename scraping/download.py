fbc551dd93146d3956ff2eac4c44324b2ae891bf
pstetz <pstetz@berkeley.edu>
2021-08-29 20:16:33 -0700

    refactor downloading script

--- scraping/download.py
+++ scraping/download.py
@@ -3,16 +3,35 @@ import time
 import json
 from pathlib import Path
 import multiprocessing as mlt
-import numpy as np
-import pandas as pd
 import urllib
-import requests
 from bs4 import BeautifulSoup
 from tqdm.auto import tqdm
+
 
 def download_url(url):
     page = urllib.request.urlopen(url)
     return BeautifulSoup(page, features="lxml")
+
+
+def find_max(url_dir):
+    """
+    Going on PBS Newshour, there is a bar that only goes up to 500, however
+    don't trust it! If you put in higher page numbers in the URL, you can
+    go back further in time. It'll still give you a URL if you've exceeded
+    and it will say "There are no videos that match the filter criteria."
+    """
+    regex = "There are no videos that match the filter criteria."
+    # HACK: assume around 2,000 pages currently. In the future max 20,000
+    # pages. An excellent example of using binary search to save time!
+    start, end = 2000, 20000
+    while start < end:
+        guess = start + ((end - start) // 2)
+        if str(download_url(f"{url_dir}/{guess}")).find(regex) == -1:
+            start = guess + 1
+        else:
+            end = guess
+    print("Guessed max page number:", start - 1)
+    return start - 1
 
 
 def get_story(url):
@@ -37,9 +56,9 @@ def get_story(url):
     # Some clips don't have a summary story
     return None
 
+
 def get_date(page):
-    date = page.find("time").text
-    return date
+    return page.find("time").text
 
 
 def get_speeches(transcript):
@@ -47,8 +66,9 @@ def get_speeches(transcript):
     for speech in transcript:
         paragraphs = speech.find_all("p")
 
-        if paragraphs[0].find("strong") != None:
-            speaker = paragraphs[0].find("strong").text.replace(":", "").split(",")[0]
+        if paragraphs[0].find("strong") is not None:
+            speaker = (paragraphs[0].find("strong").text.replace(":", "")
+                       .split(",")[0])
 
             # This allows for some mistakes, see edge case (1) below
 #             if not speaker.isupper():
@@ -62,6 +82,7 @@ def get_speeches(transcript):
 
     return speeches, list(speakers)
 
+
 def get_transcript(page):
     transcript = page.find("div", {"id": "transcript"})
 
@@ -69,36 +90,44 @@ def get_transcript(page):
         return None, None
 
     transcript = transcript.find_all("li")
-
     return get_speeches(transcript)
+
 
 # Gets all the information from a video clip
 def get_single_info(url):
     page = download_url(f"{url}#transcript")
 
     story = get_story(url)
-    date  = get_date(page)
+    date = get_date(page)
     transcript, speakers = get_transcript(page)
 
     return story, date, transcript, speakers
 
+
+def is_bad_url(url):
+    root = "https://www.pbs.org/newshour"
+    if url == f"{root}/show/0":
+        return True
+    ignore = ["politics", "nation", "world"]
+    if url.replace(f"{root}/", "").split("/")[0] in ignore:
+        return True
+    return False
+
+
 # Loops through each video on a page and stores each information
-def get_multi_info(videos, page):
+def get_multi_info(videos, page, dst_dir):
+    dst_dir = dst_dir / str(page)
     for video in videos:
         is_full = video.find_all(text=re.compile('Full Episode'))
         if len(is_full) == 1:
             continue
         video = video.find("a", {"class": "card-sm__title"})
 
-        url   = video["href"]
+        url = video["href"]
         title = video.span.text
 
-        if url == "https://www.pbs.org/newshour/show/0":
+        if is_bad_url(url):
             continue
-
-        if url.replace("https://www.pbs.org/newshour/", "").split("/")[0] in ["politics", "nation", "world"]:
-            continue
-
         try:
             story, date, transcript, speakers = get_single_info(url)
 
@@ -110,8 +139,7 @@ def get_multi_info(videos, page):
                 "transcript": transcript,
                 "speakers": speakers
             }
-            root = Path("/Users/pbezuhov/Desktop/pbs")
-            fp = root / str(page) / (url.split("/")[-1] + ".json")
+            fp = dst_dir / (url.split("/")[-1] + ".json")
             fp.parent.mkdir(exist_ok=True,  parents=True)
             with open(fp, "w") as f:
                 json.dump(news_story, f)
@@ -119,73 +147,51 @@ def get_multi_info(videos, page):
             print(e)
             print(url)
 
-def scrape_page(url_dir, i):
-    page = download_url(f"{url_dir}/{i}")
+
+def scrape_page(url_dir, page, dst_dir):
+    data = download_url(f"{url_dir}/{page}")
 
     # Display pulled video titles
-    videos = page.find("div", {"id": "all-videos"})
+    videos = data.find("div", {"id": "all-videos"})
     videos = videos.findAll("article", {"class": "card-sm"})
 
-    get_multi_info(videos, i)
+    get_multi_info(videos, page, dst_dir)
 
-### A multiprocessing threading
-def scrape_threading(url_dir, THREADS, START, DURATION):
-    manager = mlt.Manager() # multiprocessing manager
+
+def scrape_threading(url_dir, dst_dir, THREADS, START, DURATION):
+    """
+    A multiprocessing threading
+    """
     procs = []
-    pbar = tqdm(total=DURATION) # progress bar
+    pbar = tqdm(total=DURATION)  # progress bar
 
-    # Splits all the pages needed into even lists.  Numpy's amazing!
-    grouped_pages = list(range(START, START+DURATION + 1))
-    while len(grouped_pages):
+    pages = list(range(START, START+DURATION + 1))
+    while len(pages):
         procs = [p for p in procs if p.is_alive()]
         if len(procs) > THREADS:
             time.sleep(2)
             continue
-        page = grouped_pages.pop()
+        page = pages.pop()
         pbar.update(1)
-        p = mlt.Process(target=scrape_page, args=(url_dir, page))
+        p = mlt.Process(target=scrape_page, args=(url_dir, page, dst_dir))
         procs.append(p)
         p.start()
 
     [p.join() for p in procs]  # Join all threads
     pbar.close()  # End progress bar
-    return df_dict
 
 
-def find_max(url_dir):
-    """
-    Going on PBS Newshour, there is a bar that only goes up to 500, however
-    don't trust it! If you put in higher page numbers in the URL, you can
-    go back further in time. It'll still give you a URL if you've exceeded
-    and it will say "There are no videos that match the filter criteria."
-    """
-    regex = "There are no videos that match the filter criteria."
-    # HACK: assume around 2,000 pages currently. In the future max 20,000
-    # pages. An excellent example of using binary search to save time!
-    start, end = 2000, 20000
-    while start < end:
-        guess = start + ((end - start) // 2)
-        if download_url(f"{url_dir}/{guess}").find(regex) == -1:
-            start = guess + 1
-        else:
-            end = guess
-    print("Guessed max page number:", start - 1)
-    return start - 1
+def main(url_dir, dst_dir, START=1, DURATION=None, THREADS=20):
+    if DURATION is None:
+        DURATION = find_max(url_dir)  # How many pages to scrape
 
+    scrape_threading(url_dir, dst_dir, THREADS, START, DURATION)
 
-
-def main():
-    url_dir  = "https://www.pbs.org/newshour/video/page"
-    START    = 1  # Start at this page
-    DURATION = find_max(url_dir)  # How many pages to scrape
-    THREADS  = 20
-
-    pbs_df = scrape_threading(url_dir, THREADS, START, DURATION)
-    with open("../data/PBS_full_unedited.json", "w") as f:
-        json.dump(pbs_df, f, indent=2)
 
 if __name__ == "__main__":
-    main()
+    url_dir = "https://www.pbs.org/newshour/video/page"
+    dst_dir = Path("/Users/pbezuhov/Desktop/pbs")
+    main(url_dir, dst_dir)
 
 
 """
